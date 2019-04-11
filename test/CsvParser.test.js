88340e24d4a071a9b7dffff44e283a9bd52a35a3
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2019-04-11 18:54:52 +0300

    Move data of test to the description where it is used

--- test/CsvParser.test.js
+++ test/CsvParser.test.js
@@ -10,101 +10,6 @@ import {
 } from '../src/helpers';
 
 describe('makeRecords\n', () => {
-  const testDataList = {
-    makeRecords: [
-      {
-        csv: '',
-        it: '1 record x 1 field',
-        records: [
-          [
-            [['', 0], ['', 0], ['', 0], ['', 0]],
-          ],
-        ],
-      },
-      {
-        csv: '""',
-        it: '1 record x 1 field with quoted value',
-        records: [
-          [
-            [['""', 0], ['', 0], ['""', 0], ['', 2]],
-          ],
-        ],
-      },
-      {
-        csv: ',',
-        it: '1 record x 2 fields',
-        records: [
-          [
-            [['', 0], ['', 0], ['', 0], ['', 0]],
-            [[',', 0], [',', 0], ['', 1], ['', 1]],
-          ],
-        ],
-      },
-      {
-        csv: '","',
-        it: '1 record x 1 field with quoted value',
-        records: [
-          [
-            [['","', 0], ['', 0], ['","', 0], ['', 3]],
-          ],
-        ],
-      },
-      {
-        csv: '\r\n',
-        it: '2 records x 1 field',
-        records: [
-          [
-            [['', 0], ['', 0], ['', 0], ['', 0]],
-          ], [
-            [['\r\n', 0], ['\r\n', 0], ['', 2], ['', 2]],
-          ],
-        ],
-      },
-      {
-        csv: '\r\n\r\n',
-        it: '3 records x 1 field',
-        records: [
-          [[['', 0], ['', 0], ['', 0], ['', 0]]],
-          [[['\r\n', 0], ['\r\n', 0], ['', 2], ['', 2]]],
-          [[['\r\n', 2], ['\r\n', 0], ['', 2], ['', 2]]],
-        ],
-      },
-      {
-        csv: '"\r\n\r\n"',
-        it: '1 record x 1 field with quoted value',
-        records: [
-          [[['"\r\n\r\n"', 0], ['', 0], ['"\r\n\r\n"', 0], ['', 6]]],
-        ],
-      },
-      {
-        csv: ',\r\n',
-        it: '1 record x 2 fields + 1 record x 1 field',
-        records: [
-          [
-            [['', 0], ['', 0], ['', 0], ['', 0]],
-            [[',', 0], [',', 0], ['', 1], ['', 1]],
-          ],
-          [[['\r\n', 1], ['\r\n', 0], ['', 2], ['', 2]]],
-        ],
-      },
-      {
-        csv: '\n\r"abc"\r\r\n',
-        it: '1 record x 1 corrupted field + 1 record x 1 normal field',
-        records: [
-          [[['\n\r"abc"\r', 0], ['', 0], ['\n\r', 0], ['"abc"\r', 2]]],
-          [[['\r\n', 8], ['\r\n', 0], ['', 2], ['', 2]]],
-        ],
-      },
-      {
-        csv: '"abc"\n\r\r\n\n\r"abc"',
-        it: '2 records x 1 corrupted field',
-        records: [
-          [[['"abc"\n\r', 0], ['', 0], ['"abc"', 0], ['\n\r', 5]]],
-          [[['\r\n\n\r"abc"', 7], ['\r\n', 0], ['\n\r', 2], ['"abc"', 4]]],
-        ],
-      },
-    ],
-  };
   describe('throws error\n', () => {
     it(
       'when called without argument\n',
@@ -112,6 +17,101 @@ describe('makeRecords\n', () => {
     );
   });
   describe('returns set of records that contains:\n', () => {
+    const testDataList = {
+      makeRecords: [
+        {
+          csv: '',
+          it: '1 record x 1 field',
+          records: [
+            [
+              [['', 0], ['', 0], ['', 0], ['', 0]],
+            ],
+          ],
+        },
+        {
+          csv: '""',
+          it: '1 record x 1 field with quoted value',
+          records: [
+            [
+              [['""', 0], ['', 0], ['""', 0], ['', 2]],
+            ],
+          ],
+        },
+        {
+          csv: ',',
+          it: '1 record x 2 fields',
+          records: [
+            [
+              [['', 0], ['', 0], ['', 0], ['', 0]],
+              [[',', 0], [',', 0], ['', 1], ['', 1]],
+            ],
+          ],
+        },
+        {
+          csv: '","',
+          it: '1 record x 1 field with quoted value',
+          records: [
+            [
+              [['","', 0], ['', 0], ['","', 0], ['', 3]],
+            ],
+          ],
+        },
+        {
+          csv: '\r\n',
+          it: '2 records x 1 field',
+          records: [
+            [
+              [['', 0], ['', 0], ['', 0], ['', 0]],
+            ], [
+              [['\r\n', 0], ['\r\n', 0], ['', 2], ['', 2]],
+            ],
+          ],
+        },
+        {
+          csv: '\r\n\r\n',
+          it: '3 records x 1 field',
+          records: [
+            [[['', 0], ['', 0], ['', 0], ['', 0]]],
+            [[['\r\n', 0], ['\r\n', 0], ['', 2], ['', 2]]],
+            [[['\r\n', 2], ['\r\n', 0], ['', 2], ['', 2]]],
+          ],
+        },
+        {
+          csv: '"\r\n\r\n"',
+          it: '1 record x 1 field with quoted value',
+          records: [
+            [[['"\r\n\r\n"', 0], ['', 0], ['"\r\n\r\n"', 0], ['', 6]]],
+          ],
+        },
+        {
+          csv: ',\r\n',
+          it: '1 record x 2 fields + 1 record x 1 field',
+          records: [
+            [
+              [['', 0], ['', 0], ['', 0], ['', 0]],
+              [[',', 0], [',', 0], ['', 1], ['', 1]],
+            ],
+            [[['\r\n', 1], ['\r\n', 0], ['', 2], ['', 2]]],
+          ],
+        },
+        {
+          csv: '\n\r"abc"\r\r\n',
+          it: '1 record x 1 corrupted field + 1 record x 1 normal field',
+          records: [
+            [[['\n\r"abc"\r', 0], ['', 0], ['\n\r', 0], ['"abc"\r', 2]]],
+            [[['\r\n', 8], ['\r\n', 0], ['', 2], ['', 2]]],
+          ],
+        },
+        {
+          csv: '"abc"\n\r\r\n\n\r"abc"',
+          it: '2 records x 1 corrupted field',
+          records: [
+            [[['"abc"\n\r', 0], ['', 0], ['"abc"', 0], ['\n\r', 5]]],
+            [[['\r\n\n\r"abc"', 7], ['\r\n', 0], ['\n\r', 2], ['"abc"', 4]]],
+          ],
+        },
+      ],
+    };
     testDataList.makeRecords.forEach((testData) => {
       it(
         `${testData.it}\n${indent('  ', 5)}when argument 'csv' is '${escapeNL(testData.csv)}'\n`,
